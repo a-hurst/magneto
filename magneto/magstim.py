57d937ec5ea03767b58d958350f91a9c558b9f47
Austin Hurst <mynameisaustinhurst@gmail.com>
2023-01-27 20:14:39 -0400

    Add initial partial docs for main class

--- magneto/magstim.py
+++ magneto/magstim.py
@@ -7,33 +7,67 @@ from .utils import build_command, get_mode_byte, int_to_ascii, _validate_respons
 from .communication import Response, MagstimStatus, _serial_connect, comm_loop
 
 
-class Magstim(object):
 
-    def __init__(self, port):
+class Magstim(object):
+    """A connection to a Magstim stimulator.
+
+    This class is designed to be model-independent, meaning that it can be used with
+    Magstim 200, BiStim, and (eventually) Rapid stimulators. This is so that experiments
+    that do not require model-specific functionality (e.g. paired pulses or rTMS) can be
+    easily used across different Magstims in different labs without extra tweaking or
+    configuration.
+    
+    Experiments that require specific BiStim or Rapid functionality should use the
+    :class:`BiStim` or :class:`Rapid` classes instead (not yet implemented).
+    
+    Args:
+        port (str): The address of the serial port connected to the Magstim
+            (e.g. '/dev/ttyUSB0' for Linux or 'COM1' for Windows).
+        bistim_sd (bool, optional): Whether to use simultaneous discharge mode
+            instead of paired-pulse mode when connecting to a BiStim unit. Has no
+            effect on Magstim 200 or Rapid stimulators. Defaults to True.
+
+    """
+    def __init__(self, port, bistim_sd=True):
         self._port = port
         self._status = None
         self._to_stim = Queue()
         self._from_stim = Queue()
         self._com_thread = None
+        self._simultaneous = bistim_sd
 
     def connect(self):
+        """Initializes the connection to the stimulator.
+        
+        This method initializes serial communications with the Magstim and enables
+        remote control of the hardware. Must be called before any other methods or
+        attributes can be used.
+
+        """
+        # TODO: Add logic for ensuring connection exists in other methods/attrs
+        # Initializes the Magstim serial communication thread
         self._com_thread = threading.Thread(
             target=comm_loop,
             args=(self._port, self._from_stim, self._to_stim),
             daemon=True
         )
         self._com_thread.start()
+
+        # Request computer control over the Magstim
         self._enable_remote_control()
+
         # If stimulator is a BiStim, configure it into single-pulse mode
         try:
-            # TODO: Add an option for choosing type of BiStim single-pulse mode
-            #self._set_power_b(0)
-            #self._set_pulse_interval(1)
-            self._set_pulse_interval(0)
+            if self._simultaneous:
+                self._set_pulse_interval(0)
+            else:
+                self._set_pulse_interval(1)
+                self._set_power_b(0)
         except RuntimeError:
             pass
 
     def _pump(self):
+        # Pumps the TMS input queue for new response packets
         out = []
         while not self._from_stim.empty():
             resp_bytes = self._from_stim.get()
@@ -44,9 +78,11 @@ class Magstim(object):
         return out
 
     def _send_cmd(self, cmd, data=None):
+        # Sends a command to the Magstim
         self._to_stim.put(build_command(cmd, data))
 
     def _wait_for_reply(self, cmd, timeout=1.0):
+        # Waits for a response packet corresponding to a given command
         start = time.time()
         while (time.time() - start) < timeout:
             for resp in self._pump():
@@ -62,12 +98,29 @@ class Magstim(object):
         return resp.status
 
     def _enable_remote_control(self):
+        # Enables remote control over the stimulator. Unsure if this should be public.
         return self._communicate(ENABLE_REMOTE_CTRL)
 
     def _disable_remote_control(self):
+        # Disables remote control over the stimulator. Unsure if this should be public.
         return self._communicate(DISABLE_REMOTE_CTRL)
 
     def set_power(self, value):
+        """Sets the power level for the primary coil of the stimulator.
+
+        Note that the stimulator requires time to charge or discharge to a new power
+        level once one has been set, with the amount of time depending on the magnitude
+        and direction of the change (e.g. 20% to 40% would normally take ~200 ms,
+        whereas 40% to 20% would take ~2000 ms).
+
+        To make sure the stimulator is ready to fire after changing the power level,
+        check the `ready` attribute once the stimulator has been armed.
+
+        Args:
+            level (int): The power level (from 0 to 100) to set for the stimulator's
+                primary coil.
+
+        """
         if not (0 <= value <= 100):
             e = "Power level must be an integer between 0 and 100 (got {0})"
             raise ValueError(e.format(value))
@@ -75,14 +128,38 @@ class Magstim(object):
         return self._communicate(SET_POWER_A, level)
 
     def arm(self):
+        """Arms the stimulator.
+
+        Once armed, the Magstim will disarm automatically if the stimulator has not
+        been fired for over 1 minute. Please ensure that your code is written with
+        this constraint in mind.
+
+        """
         armed = get_mode_byte(MODE_ARMED)
         return self._communicate(SET_BASE_MODE, armed)
     
     def disarm(self):
+        """Disarms the stimulator.
+
+        See :meth:`arm` for more information.
+        
+        """
         disarmed = get_mode_byte(0)
         return self._communicate(SET_BASE_MODE, disarmed)
     
     def fire(self):
+        """Signals the stimulator to fire.
+
+        The stimulator must be armed and ready before this method can be called.
+
+        .. note::
+           This commands the TMS to fire via the serial port, which has a variable
+           delay anywhere between 5 and 15 ms. As such, this method should usually
+           only be used for testing or for tasks where precise timing is not important.
+           In all other cases, the stimulator should be triggered via TTL using an
+           external triggering device (e.g. LabJack).
+
+        """
         trigger = get_mode_byte(MODE_TRIGGER)
         return self._communicate(SET_BASE_MODE, trigger)
 
@@ -98,6 +175,8 @@ class Magstim(object):
         return (pwr_a, pwr_b, pulse_interval)
 
     def _set_power_b(self, value):
+        # BiStim-only: sets the power level for the second pulse when in
+        # paired-pulse mode.
         if not (0 <= value <= 100):
             e = "Power level must be an integer between 0 and 100 (got {0})"
             raise ValueError(e.format(value))
@@ -105,12 +184,16 @@ class Magstim(object):
         return self._communicate(SET_POWER_B, level)
 
     def _set_highres_time(self, enable=True):
+        # BiStim-only: toggles whether the paired pulse interval is in units of
+        # 0.1 ms (high-resolution enabled) or 1.0 ms (high-resolution disabled)
         if enable:
             return self._communicate(ENABLE_HIRES_TIME)
         else:
             return self._communicate(DISABLE_HIRES_TIME)
 
     def _set_pulse_interval(self, value):
+        # BiStim-only: sets the paired-pulse interval for the stimulator
+        # NOTE: Setting the interval to 0 enables simultaneous-discharge mode
         # NOTE: No obvious way to check current pulse interval resolution
         if not (0 <= value <= 100):
             e = "Pulse interval must be a value between 0 and 100 (got {0})"
@@ -120,6 +203,9 @@ class Magstim(object):
 
     @property
     def armed(self):
+        """bool: True if the stimulator is currently armed, otherwise False.
+
+        """
         # NOTE: When magstim is ready 'armed' bit is set to 0, so need to check both
         self._send_cmd(ENABLE_REMOTE_CTRL)
         resp = self._wait_for_reply(ENABLE_REMOTE_CTRL)
@@ -128,6 +214,9 @@ class Magstim(object):
     
     @property
     def ready(self):
+        """bool: True if the stimulator is ready to fire, otherwise False.
+
+        """
         self._send_cmd(ENABLE_REMOTE_CTRL)
         resp = self._wait_for_reply(ENABLE_REMOTE_CTRL)
         _validate_response(resp)
